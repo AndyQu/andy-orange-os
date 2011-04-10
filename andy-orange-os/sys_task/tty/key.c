89cb8cc55de6a1ab2d4e78876b845a18288063f4
aichongqu <aichongqu@126.com>
2011-04-10 03:13:57 +0000

    remove dump.fresh key.c

--- andy-orange-os/sys_task/tty/key.c
+++ andy-orange-os/sys_task/tty/key.c
@@ -6,121 +6,24 @@
 #include"assert.h"
 #include"video_const.h"
 
-PUBLIC	void	init_KB_INPUT(KB_INPUT* kb_buf)
-{
-	assert(kb_buf);
-	kb_buf->head=0;
-	kb_buf->tail=1;
-	kb_buf->count=0;
-}
-
-PUBLIC	char	get_scancode(KB_INPUT* kb_buf,int index)
-{
-	int a;
-	assert(kb_buf);
-	if(index>kb_buf->count||index<=0)
-		return	-1;
-	if(kb_buf->head<kb_buf->tail){
-			return	kb_buf->buf[kb_buf->head+index];
-	}
-	else if(kb_buf->head>kb_buf->tail){
-		if(kb_buf->head+index>KB_IN_BYTES-1)
-			a=kb_buf->head+index-(KB_IN_BYTES);
-		else 
-			a=kb_buf->head+index;
-		return	kb_buf->buf[a];
-
-	}
-	return -1;
-}
-
-PUBLIC	char	shift_out(KB_INPUT* p)
-{
-	assert(p);
-	if(p->count<=0)
-		return	-1;
-	if(p->head < p->tail){
-		p->head++;
-		p->count--;
-		return	p->buf[p->head];
-	}
-	else if(p->head > p->tail){
-		if(p->head==KB_IN_BYTES-1){
-			p->head=0;
-			p->count--;
-			return	p->buf[p->head];
-		}else{
-			p->head++;
-			p->count--;
-			return	p->buf[p->head];
-		}
-	}
-}
-PUBLIC	void	shift_outM(KB_INPUT*p,int c)
-{
-	int i=0;
-	for(;i<c;i++){
-		shift_out(p);
-	}
-}
-
-PUBLIC	int	add_head(KB_INPUT*p,char code)
-{
-	assert(p);
-	if(p->count>=KB_IN_BYTES-2)
-		return	FALSE;
-	if(p->head<p->tail){
-		if(p->head==0){
-			p->buf[0]=code;
-			p->head=KB_IN_BYTES-1;
-			p->count++;
-		}
-		else{
-			p->buf[p->head]=code;
-			p->head--;
-			p->count++;
-		}
-	}
-	else if(p->head>p->tail){
-		p->buf[p->head]=code;
-		p->head--;
-		p->count++;
-	}
-	return TRUE;
-}
-
-
-PUBLIC	int	add_tail(KB_INPUT*p,char code)
-{
-	assert(p);
-	if(p->count>=KB_IN_BYTES-2)
-		return	FALSE;
-	if(p->head<p->tail){
-		if(p->tail==KB_IN_BYTES-1){
-			p->buf[p->tail]=code;
-			p->tail=0;
-			p->count++;
-		}
-		else{
-			p->buf[p->tail]=code;
-			p->tail++;
-			p->count++;
-		}
-	}
-	else if(p->head>p->tail){
-		p->buf[p->tail]=code;
-		p->tail++;
-		p->count++;
-	}
-	return TRUE;
-}
-
-
-
-
-/*下面这个函数作为键盘输入的中断相应函数*/
 extern	int disp_pos;
+extern	int	__currentTTY;
 PRIVATE	KB_INPUT	kb_input[1];
+PRIVATE	t_bool	l_shift;
+PRIVATE	t_bool	r_shift;
+PRIVATE	t_bool	l_ctrl;
+PRIVATE	t_bool	r_ctrl;
+PRIVATE	t_bool	l_alt;
+PRIVATE	t_bool	r_alt;
+void	init_KB_INPUT(KB_INPUT* kb_buf);
+char	get_scancode(KB_INPUT* kb_buf,int index);
+char	shift_out(KB_INPUT* p);
+void	shift_outM(KB_INPUT*p,int c);
+int	add_head(KB_INPUT*p,char code);
+int	add_tail(KB_INPUT*p,char code);
+int	printChar(int tty,t_32 key);
+void set_cursor(int pos);
+void	init_KB_INPUT(KB_INPUT* kb_buf);
 /*
 RING<0>
 */
@@ -131,14 +34,6 @@ PUBLIC	void key_interrupt()
 	value=in_byte(0x60);
 	add_tail(kb_input,value);
 }
-
-PRIVATE	t_bool	l_shift;
-PRIVATE	t_bool	r_shift;
-PRIVATE	t_bool	l_ctrl;
-PRIVATE	t_bool	r_ctrl;
-PRIVATE	t_bool	l_alt;
-PRIVATE	t_bool	r_alt;
-
 /*
 RING<0>
 */
@@ -152,8 +47,6 @@ PUBLIC	void	init_keyboard()
 	l_alt=FALSE;
 	r_alt=FALSE;
 }
-extern	int	__currentTTY;
-int	printChar(int tty,t_32 key);
 /*
 RING<1>
 */
@@ -316,21 +209,6 @@ PUBLIC	int	keyboard_read(TTY *p)
 	return	-1;
 }
 
-PUBLIC void set_cursor(int pos)
-{
-
-	out_byte(CRTC_ADDR_REG,CRTC_DATA_IDX_CURSOR_H);
-	out_byte(CRTC_DATA_REG,(pos>>8)& 0xff);
-	out_byte(CRTC_ADDR_REG,CRTC_DATA_IDX_CURSOR_L);
-	out_byte(CRTC_DATA_REG,(pos)& 0xff);
-}
-PUBLIC void	set_disp_addr(int addr)
-{
-	out_byte(CRTC_ADDR_REG,CRTC_DATA_IDX_START_ADDR_H);
-	out_byte(CRTC_DATA_REG,(addr>>8)&0xff);
-	out_byte(CRTC_ADDR_REG,CRTC_DATA_IDX_START_ADDR_L);
-	out_byte(CRTC_DATA_REG,(addr)&0xff);
-}
 
 TTY* id2tty(int);
 /*
@@ -342,7 +220,6 @@ PUBLIC int printChar(int tty_id,t_32 key)
 	char *output="1";
 	int a=key&FLAG_EXT;
 	
-	assert(tty_id>=0 && tty_id<=2);
 	TTY*p_tty=id2tty(tty_id);
 	
 	/*if 超出显示范围，则从头开始显示*/
@@ -404,9 +281,9 @@ PUBLIC int printChar(int tty_id,t_32 key)
 	//enable_int();
 	return ret;
 }
-
-
-
+/*
+RING<0><1>
+*/
 PUBLIC void	printStr(int tty_id,char*s)
 {
 	int i;
@@ -414,10 +291,133 @@ PUBLIC void	printStr(int tty_id,char*s)
 		printChar(tty_id, (t_32)s[i]);
 	}
 }
-PUBLIC void	printStrLen(int tty_id,char*s,int len)
+/*
+RING<0><1>
+*/
+PUBLIC void set_cursor(int pos)
 {
-	int i;
-	for(i=0;i<len && s[i]!=0;i++){
-		printChar(tty_id, (t_32)s[i]);
+
+	out_byte(CRTC_ADDR_REG,CRTC_DATA_IDX_CURSOR_H);
+	out_byte(CRTC_DATA_REG,(pos>>8)& 0xff);
+	out_byte(CRTC_ADDR_REG,CRTC_DATA_IDX_CURSOR_L);
+	out_byte(CRTC_DATA_REG,(pos)& 0xff);
+}
+/*
+RING<0><1>
+*/
+PUBLIC void	set_disp_addr(int addr)
+{
+	out_byte(CRTC_ADDR_REG,CRTC_DATA_IDX_START_ADDR_H);
+	out_byte(CRTC_DATA_REG,(addr>>8)&0xff);
+	out_byte(CRTC_ADDR_REG,CRTC_DATA_IDX_START_ADDR_L);
+	out_byte(CRTC_DATA_REG,(addr)&0xff);
+}
+/*Below are utility functions*/
+PUBLIC	void	init_KB_INPUT(KB_INPUT* kb_buf)
+{
+	assert(kb_buf);
+	kb_buf->head=0;
+	kb_buf->tail=1;
+	kb_buf->count=0;
+}
+
+PUBLIC	char	get_scancode(KB_INPUT* kb_buf,int index)
+{
+	int a;
+	assert(kb_buf);
+	if(index>kb_buf->count||index<=0)
+		return	-1;
+	if(kb_buf->head<kb_buf->tail){
+			return	kb_buf->buf[kb_buf->head+index];
 	}
+	else if(kb_buf->head>kb_buf->tail){
+		if(kb_buf->head+index>KB_IN_BYTES-1)
+			a=kb_buf->head+index-(KB_IN_BYTES);
+		else 
+			a=kb_buf->head+index;
+		return	kb_buf->buf[a];
+
+	}
+	return -1;
+}
+
+PUBLIC	char	shift_out(KB_INPUT* p)
+{
+	assert(p);
+	if(p->count<=0)
+		return	-1;
+	if(p->head < p->tail){
+		p->head++;
+		p->count--;
+		return	p->buf[p->head];
+	}
+	else if(p->head > p->tail){
+		if(p->head==KB_IN_BYTES-1){
+			p->head=0;
+			p->count--;
+			return	p->buf[p->head];
+		}else{
+			p->head++;
+			p->count--;
+			return	p->buf[p->head];
+		}
+	}
+}
+PUBLIC	void	shift_outM(KB_INPUT*p,int c)
+{
+	int i=0;
+	for(;i<c;i++){
+		shift_out(p);
+	}
+}
+
+PUBLIC	int	add_head(KB_INPUT*p,char code)
+{
+	assert(p);
+	if(p->count>=KB_IN_BYTES-2)
+		return	FALSE;
+	if(p->head<p->tail){
+		if(p->head==0){
+			p->buf[0]=code;
+			p->head=KB_IN_BYTES-1;
+			p->count++;
+		}
+		else{
+			p->buf[p->head]=code;
+			p->head--;
+			p->count++;
+		}
+	}
+	else if(p->head>p->tail){
+		p->buf[p->head]=code;
+		p->head--;
+		p->count++;
+	}
+	return TRUE;
+}
+
+
+PUBLIC	int	add_tail(KB_INPUT*p,char code)
+{
+	assert(p);
+	if(p->count>=KB_IN_BYTES-2)
+		return	FALSE;
+	if(p->head<p->tail){
+		if(p->tail==KB_IN_BYTES-1){
+			p->buf[p->tail]=code;
+			p->tail=0;
+			p->count++;
+		}
+		else{
+			p->buf[p->tail]=code;
+			p->tail++;
+			p->count++;
+		}
+	}
+	else if(p->head>p->tail){
+		p->buf[p->tail]=code;
+		p->tail++;
+		p->count++;
+	}
+	return TRUE;
 }
